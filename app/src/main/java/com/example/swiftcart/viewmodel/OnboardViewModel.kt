3a98ead82e6657e737d796da1d263c75fa9a74de
Arthur <okoloarthur4@gmail.com>
2024-05-27 20:24:16 +0100

    create onboarding model and implement onboarding logic

---
+++ app/src/main/java/com/example/swiftcart/viewmodel/OnboardViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.swiftcart.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.swiftcart.data.DataStoreRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardViewModel @Inject constructor(
+    private val repository: DataStoreRepo
+) : ViewModel() {
+
+    fun saveOnBoardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.saveOnBoardingState(completed = completed)
+        }
+    }
+
+}
