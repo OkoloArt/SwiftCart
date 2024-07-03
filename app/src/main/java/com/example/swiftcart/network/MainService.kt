798b91c8465add8a9faad0c186512cc35a6c59c8
Arthur <okoloarthur4@gmail.com>
2024-07-03 03:09:02 +0100

    refactor main service name

--- app/src/main/java/com/example/swiftcart/network/MainService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.swiftcart.network
-
-import io.ktor.client.HttpClient
-import javax.inject.Inject
-import javax.inject.Named
-
-class MainService @Inject constructor(@Named("MainClient")private val client: HttpClient) {
-
-}
